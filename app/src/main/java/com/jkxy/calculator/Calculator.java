47a443dbffa790a066c9e6286ef324bd5343745a
YuWei <503551761@qq.com>
2016-10-18 11:57:38 +0800

    test

--- app/src/main/java/com/jkxy/calculator/Calculator.java
+++ app/src/main/java/com/jkxy/calculator/Calculator.java
@@ -2,9 +2,10 @@ package com.jkxy.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Calculator extends AppCompatActivity {
-    private Buttone btnOne,btnTwo,btnThree;
+    private Button btnOne,btnTwo,btnThree,btnFour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/jkxy/calculator/click.java
@@ -0,0 +1,7 @@
+package com.jkxy.calculator;
+
+/**
+ * Created by Administrator on 2016/10/18.
+ */
+public class click {
+}
